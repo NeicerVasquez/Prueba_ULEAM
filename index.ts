6f233db42aebc17c3dcbb86d53a46c50ef09d841
Neicer Vásquez <NeicerYEANYT18@gmail.com>
2023-09-18 21:52:58 -0500

    Este en un nuevo commit que lo hice con el atributo -m

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
 // Para compilar el código de un archivo .ts se debe escribir en consola ts-node <file>
 
-console.log('Aprendiendo a usar git en 2023 con \nun video de youtube que dura 5 horas')
+console.log(
+  "Aprendiendo a usar git en 2023 con \nun video de youtube que dura 5 horas"
+);
+
+console.log("Prueba de comandos");
